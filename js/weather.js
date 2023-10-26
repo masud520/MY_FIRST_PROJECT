b64488f717cf2e3a25d4203392c4b64a035ca5a6
masud520 <masud.abdi@student.moringaschool.com>
2023-10-26 12:37:06 +0300

    10th commit

--- js/weather.js
+++ js/weather.js
@@ -19,19 +19,19 @@ async function checkWeather(city) {
         document.querySelector('.wind').innerHTML = data.wind.speed + "Km/h";
 
         if (data.weather[0].main == "Clouds") {
-            weatherIcon.src = "images/clouds.png";
+            weatherIcon.src = "css/images/clouds.png";
         } else if (data.weather[0].main == "Clear") {
-            weatherIcon.src = "images/clear.png";
+            weatherIcon.src = "css/images/clear.png";
         } else if (data.weather[0].main == "Drizzle") {
-            weatherIcon.src = "images/drizzle.png";
+            weatherIcon.src = "css/images/drizzle.png";
         } else if (data.weather[0].main == "Rain") {
-            weatherIcon.src = "images/rain.png";
+            weatherIcon.src = "css/images/rain.png";
         } else if (data.weather[0].main == "Mist") {
-            weatherIcon.src = "images/mist.png";
+            weatherIcon.src = "css/images/mist.png";
         } else if (data.weather[0].main == "Snow") {
-            weatherIcon.src = "images/snow.png";
+            weatherIcon.src = "css/images/snow.png";
         } else if (data.weather[0].main == "Humidity") {
-            weatherIcon.src = "images/humidity.png";
+            weatherIcon.src = "css/images/humidity.png";
         }
         document.querySelector(".weather").style.display = "block";
         document.querySelector(".error").style.display = "none";
